66837ebb2c3aad8d9f8a5da2417bd60958acc5d1
aram-ap <aramap22@gmail.com>
2024-01-05 14:12:44 -0800

    Displays CSV File Now

--- DataAnalyzer.py
+++ DataAnalyzer.py
@@ -31,15 +31,33 @@ def testType(path: str):
 
     return dict
 
+def getCsvFile(path: str):
+    csvFile = pd.read_csv(path, sep=',', header=None)
+    return csvFile
+
+def normalize(csvFile):
+    pass
+
 def callback():
     name = fd.askopenfilename();
-    print(testType(name))
+
+    if(len(name) < 1):
+        return "No file passed"
+
+    dict = testType(name)
+    for key in dict.keys():
+        print(key, ": ", dict[key])
+
+    file = getCsvFile(name)
+    print(file)
+
+
 
 def main():
     root = tk.Tk()
     root.geometry("400x300")
     root.resizable(False,False)
-    button = tk.Button(text='Click to Open File', command=callback).pack(fill=tk.X)
+    button = tk.Button(text='Click to Open File', command=callback, height=5).pack(fill=tk.X)
     root.mainloop()
 
 if __name__ == "__main__":
